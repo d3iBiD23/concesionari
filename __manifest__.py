c8e3ff29ece6c879ced91cfa2ccfb0e397daa1f4
Davide Carrión <77077581+d3iBiD23@users.noreply.github.com>
2024-05-27 18:26:33 +0200

    Update __manifest__.py
    demo xml deleted in demostration mode

--- __manifest__.py
+++ __manifest__.py
@@ -27,9 +27,5 @@
         'views/views.xml',
         'views/templates.xml'
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
     'application': True
 }
